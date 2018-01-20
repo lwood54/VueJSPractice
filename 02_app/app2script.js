73996fea2d5a8b77d3edeadc926c1ba1acd44b86
Logan Wood <lwood3499@gmail.com>
2018-01-20 14:42:57 -0600

    first commit: VueJS introduction

---
+++ 02_app/app2script.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+  data: {
+    title: 'Hello World!',
+    link: 'http://google.com'
+  },
+  methods: {
+    sayHello: function() {
+      return this.title;
+    }
+  }
+});
